96f91e2dd48d4ac9f48338ef42ba84d08b927ff8
uniqueVlaB <bulahvlad7@gmail.com>
2024-01-26 15:31:15 +0200

    transfer to .net8 and fab animations

--- ProjectChronos/View/DeadlinesPage.xaml.cs
+++ ProjectChronos/View/DeadlinesPage.xaml.cs
@@ -4,9 +4,19 @@ namespace ProjectChronos.View;
 
 public partial class DeadlinesPage : ContentPage
 {
+	short RotateDirection = -1; //-1 left, 1 right
 	public DeadlinesPage(DeadlinesPageViewModel viewModel)
 	{
 		BindingContext = viewModel;
 		InitializeComponent();
 	}
+
+    private void FABbtn_Clicked(object sender, EventArgs e)
+    {
+		((Button)sender).RotateTo(90*RotateDirection);
+        RotateDirection *= -1;
+
+		//ActionBtnStack.IsVisible = r == -1 ? true : false;
+		ActionBtnStack.TranslateTo(63*-RotateDirection,0);
+    }
 }
